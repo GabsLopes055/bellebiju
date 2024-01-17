d16de2ff96752ac74dbcf9a029ff2eb42c38c234
GabsLopes055 <lopesgabriel055@gmail.com>
2024-01-17 18:10:37 -0300

    Permitindo o botão de pesquisar vendas

--- src/app/pages/vendas/component/list-vendas/list-vendas.component.ts
+++ src/app/pages/vendas/component/list-vendas/list-vendas.component.ts
@@ -1,3 +1,4 @@
+import { PermissionUser } from 'src/app/shared/userLogged/permissionUser.service';
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { ModelPesquisarPorDataComponent } from '../model-pesquisar-por-data/model-pesquisar-por-data.component';
@@ -9,9 +10,14 @@ import { ModelPesquisarPorDataComponent } from '../model-pesquisar-por-data/mode
 })
 export class ListVendasComponent {
 
+  permissionPesquisar!: boolean
+
   constructor(
-    private dialog: MatDialog
-  ){}
+    private dialog: MatDialog,
+    private permission: PermissionUser
+  ){
+    if(this.permission.hasPermission("ADMIN")) this.permissionPesquisar = true
+  }
 
   pesquisarPorData() {
     this.dialog.open(ModelPesquisarPorDataComponent, {
