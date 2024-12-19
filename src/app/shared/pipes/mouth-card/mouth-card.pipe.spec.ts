eb6c1ab96d080d0aeccac01f2b75f4641b035751
Gabriel Lopes Teixeira <51964786+GabsLopes055@users.noreply.github.com>
2024-12-18 22:16:36 -0300

    Delete src/app/shared/pipes directory

--- src/app/shared/pipes/mouth-card/mouth-card.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MouthCardPipe } from './mouth-card.pipe';
-
-describe('MouthCardPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MouthCardPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
