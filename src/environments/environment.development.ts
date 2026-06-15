63613a1325f5ed4a7c235e7b32353f2dc92e2845
Gabs2305 <lopesgabriel055@gmail.com>
2026-06-15 17:24:48 -0300

    fix: testando novo deploy

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
   // url: 'http://localhost:8080/api',
-  url: 'https://bellebiju-backend-production-5cda.up.railway.app/api',
+  // url: 'https://bellebiju-backend-production-5cda.up.railway.app/api',
+  url: 'https://bellebiju-backend.fly.dev/api',
 };
